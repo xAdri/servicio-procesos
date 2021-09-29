dd26a38e7da58e2bef6fe09a43b001548368d063
Adrián Pérez <adriperez@rudo.es>
2021-09-29 22:42:10 +0200

    AE1_1

---
+++ AE1/src/es/florida/ejercicios/Ejercicio_1.java
@@ -0,0 +1,10 @@
+package es.florida.ejercicios;
+
+public class Ejercicio_1 {
+	public static void main(String []args){
+        sayHello();
+    }
+    public static void sayHello(){
+        System.out.println("Hola Mundo");
+    }
+}
