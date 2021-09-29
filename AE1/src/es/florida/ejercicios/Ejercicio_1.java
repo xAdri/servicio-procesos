399461ab06dbb04b72510a3ec19e97810c629c08
Adrián Pérez <adriperez@rudo.es>
2021-09-29 22:47:46 +0200

    Comment AE1-1

--- AE1/src/es/florida/ejercicios/Ejercicio_1.java
+++ AE1/src/es/florida/ejercicios/Ejercicio_1.java
@@ -4,6 +4,9 @@ public class Ejercicio_1 {
 	public static void main(String []args){
         sayHello();
     }
+	/**
+	 * sayHello imprime por pantalla Hola Mundo
+	 */
     public static void sayHello(){
         System.out.println("Hola Mundo");
     }
