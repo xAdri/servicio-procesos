2b001a222b9d05bea7b33e1f6c65a71d2e059b9b
Adrián Pérez <adriperez@rudo.es>
2021-09-29 23:15:03 +0200

    AE1-2 Ejercicio B

--- AE1/src/es/florida/ejercicios/Ejercicio_2.java
+++ AE1/src/es/florida/ejercicios/Ejercicio_2.java
@@ -1,8 +1,11 @@
 package es.florida.ejercicios;
 
+import java.util.ArrayList;
+
 public class Ejercicio_2 {
 	public static void main(String[] args) {
 		EjercicioA();
+		EjercicioB();
 	}
 	/**
 	 * Escribe por consola las lineas consecutivas
@@ -12,6 +15,23 @@ public class Ejercicio_2 {
 		
 		for (int i = 0; i < array.length; i++) {
 			System.out.println(array[i]);
+		}
+    }
+	/**
+	 * Escribe por consola las lineas consecutivas del arraylist
+	 */
+	public static void EjercicioB(){
+		ArrayList<String> arrayList = new ArrayList<String>();
+		
+		arrayList.add("Adri");
+		arrayList.add("David");
+		arrayList.add("Manu");
+		arrayList.add("Carla");
+		arrayList.add("Erika");
+		arrayList.add("Pedro");
+		
+		for (int i = 0; i < arrayList.size(); i++) {
+			System.out.println(arrayList.get(i));
 		}
     }
 }
