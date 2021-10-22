77d268662752a6e758406807101db039b7bc526b
Adrián Pérez <adriperez@rudo.es>
2021-10-22 20:33:19 +0200

    Create NEO Help functionality

---
+++ AE2/src/es/florida/ejercicios/Launcher.java
@@ -0,0 +1,84 @@
+package es.florida.ejercicios;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Launcher {
+
+    public static void main(String[] args) throws IOException {
+    	System.out.println("Starting NEO Help!");
+    	System.out.println("[Processors available from this pc: " + getCores() + "]");
+        
+        long start_time = System.currentTimeMillis();
+        readNeosLaunchProbability();
+        long end_time = System.currentTimeMillis();
+        double miliseconds = (double)(end_time - start_time);
+        System.out.println("Total execution time of the program: " + (miliseconds/1000) + " seconds.");
+    	System.out.println("Thanks for use NEO Help!");
+        
+
+    }
+    
+    static void readNeosLaunchProbability() throws IOException {
+    	File f = null;
+        FileReader fr = null;
+        BufferedReader br = null;
+
+        try {
+            f = new File("NEOs.txt");
+            fr = new FileReader(f);
+            br = new BufferedReader(fr);
+            String line;
+
+            for (int i = 0; i < getCores(); i++) {
+                line = br.readLine();
+                System.out.println("==> Doing the calculations for: " + line);
+                String[] neoData = line.split(",");
+                launchProbability( neoData[0], neoData[1], neoData[2]);
+            }
+
+        } catch (Exception e) {
+
+        } finally {
+            fr.close();
+        }
+    }
+    
+    static int getCores() {
+    	return Runtime.getRuntime().availableProcessors();
+    }
+    
+    static void launchProbability(String fRes, String posNEO, String velNEO) {
+        String cName = "es.florida.ejercicios.Probability";
+        
+        try {
+
+            String javaHome = System.getProperty("java.home");
+            String javaBin = javaHome + File.separator + "bin" + File.separator + "java";
+            String classpath = System.getProperty("java.class.path");
+            String className = cName;
+
+            List < String > command = new ArrayList < > ();
+            command.add(javaBin);
+            command.add("-cp");
+            command.add(classpath);
+            command.add(className);
+            command.add(posNEO.toString());
+            command.add(velNEO.toString());
+            command.add(fRes.toString());
+
+            ProcessBuilder builder = new ProcessBuilder(command);
+            Process process = builder.inheritIO().start();
+            process.waitFor();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ AE2/src/es/florida/ejercicios/Probability.java
@@ -0,0 +1,67 @@
+package es.florida.ejercicios;
+
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+
+public class Probability {
+    
+    public static void main(String[] args) {
+        Probability prob = new Probability();
+        double posEarth = Float.parseFloat(args[0]);
+        double velEarth = Float.parseFloat(args[1]);
+        probabilityNeoCollision(prob, posEarth, velEarth, args);
+        
+    }
+    
+    static void probabilityNeoCollision(Probability prob, double posEarth, double velEarth, String[] args) {
+    	FileWriter fw = null;
+        PrintWriter pw = null;
+        try {
+            fw = new FileWriter(args[2]);
+            pw = new PrintWriter(fw);
+            double result = prob.probabilityColision(posEarth, velEarth);
+            pw.println(result);
+            System.out.println("The probability that it will collide with the earth in the next 50 years is " + result  + "%");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (null != fw)
+                    fw.close();
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+        }
+    }
+    
+    public double probabilityColision(double posNEO, double velNEO) {
+        double posEarth = 1;
+        double velEarth = 100;
+        
+        long start_time = System.currentTimeMillis();
+        
+        for (int i = 0; i < (50 * 365 * 24 * 60 * 60); i++) {
+            posNEO = posNEO + velNEO * i;
+            posEarth = posEarth + velEarth * i;
+        }
+
+        double result = 100 * Math.random() * Math.pow(((posNEO - posEarth) / (posNEO + posEarth)), 2);
+        BigDecimal bigDecimal = new BigDecimal(result).setScale(2, RoundingMode.UP);
+
+        long end_time = System.currentTimeMillis();
+        double miliseconds = (double)(end_time - start_time) ;
+        
+        System.out.println("Time consumed of the calculation execution: " + (miliseconds/1000) + " seconds");
+        if (result > 10) {
+        	 System.err.println("GLOBAL ALERT, POSSIBLE NEO COLLISION WITH EARTH!!");
+        } else {
+            System.out.println("Everything is going well, there is no risk of NEO collision with the Earth.");
+        }
+        
+        return bigDecimal.doubleValue();
+    }
+}
